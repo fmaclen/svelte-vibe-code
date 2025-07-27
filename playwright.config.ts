75cade93bdc1321dc60e8c88c57b94914f39a211
Fernando Maclen <fmaclen@gmail.com>
2025-07-27 14:30:20 -0400

    Configure Playwright to spawn PocketBase server during tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+	webServer: [
+		{
+			command: 'npx pocketbase serve --dir=./pb_test_data',
+			port: 8090,
+			reuseExistingServer: !process.env.CI,
+			timeout: 120000,
+		},
+		{
+			command: 'npm run build && npm run preview',
+			port: 4173,
+			reuseExistingServer: !process.env.CI,
+		}
+	],
+	testDir: 'e2e',
+	use: {
+		baseURL: 'http://localhost:4173',
 	},
-	testDir: 'e2e'
 });
